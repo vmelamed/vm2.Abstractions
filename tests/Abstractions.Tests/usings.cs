66b4d955dd711831c9596c1cacb3ce87f1b5075b
Val Melamed <valo@vtmelamed.com>
2026-06-07 19:45:24 -0400

    refactor: tenant management interfaces and implementations
    - Updated ICurrentTenant and ITenanted interfaces to clarify tenant identifier requirements and behavior.
    - Enhanced documentation for tenant management, emphasizing sentinel values and context handling.
    - Modified CurrentTenant implementation to improve clarity and ensure proper handling of tenant identifiers.
    - Adjusted unit tests to reflect changes in tenant behavior, ensuring correct handling of default values.
    - Removed unnecessary package dependencies from project and test configurations.

--- tests/Abstractions.Tests/usings.cs
+++ tests/Abstractions.Tests/usings.cs
@@ -1,12 +1,5 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025-2026 Val Melamed
 
-global using System.Diagnostics.CodeAnalysis;
-global using System.Runtime.Serialization;
-global using System.Text;
-
 global using vm2.TestUtilities;
-global using vm2.Abstractions;
 global using vm2.Abstractions.Providers;
-
-global using static vm2.TestUtilities.TestUtilities;
